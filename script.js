dee57c6f9193bff82ccc530ee264e6c79b25412c
uta7770502 <yutakiyama@gmail.com>
2025-10-12 17:06:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function initMap(){
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
   }).addTo(map);
+  window.mapInstance = map;
 
   // ピンとポップアップ
   window.COACHES.forEach(c => {
